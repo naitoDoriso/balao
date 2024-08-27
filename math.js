0215bb06a82df2b6e4dd6ba4549fe3d66cf5185d
naitoDoriso <89417602+naitoDoriso@users.noreply.github.com>
2024-08-27 19:33:21 -0300

    Notação científica adicioanda e pow otimizado

--- math.js
+++ math.js
@@ -4,10 +4,9 @@ function BigMult(s, t){
 
     for (let i=0; i<s.length+t.length-1; i++) dig[i] = 0;
 
-    for (let i=0; i<s.length; i++){
-        for (let j=0; j<t.length; j++){
+    for (let i=s.length-1; i>=0; i--){
+        for (let j=t.length-1; j>=0; j--){
             dig[i+j] += (s[i]-0)*(t[j]-0);
-
             if (dig[i+j] > 9){
             	if (i+j == 0) r += parseInt(dig[i+j]/10);
             	else dig[i+j-1] += parseInt(dig[i+j]/10);
@@ -48,13 +47,51 @@ function BigInc(s){
 	return dig.join("");
 }
 
+function BigDivInt(s, n){
+	let dig = [];
+	for (let i=0; i<s.length; i++) dig[i] = 0;
+
+	for (let i=0; i<s.length; i++) {
+		dig[i] += (s[i]-'0')/n;
+		if (i < s.length-1) dig[i+1] += (dig[i]%1)*10;
+		dig[i] = parseInt(dig[i]);
+	}
+	if (dig.length > 1 && dig[0] == 0) {
+		dig.splice(0,1);
+	}
+	return dig.join("");
+}
+
+function BigPar(s){
+	return (s[s.length-1]-'0')%2;
+}
+
+function cmp(s, t){
+	let ans = '';
+	if (s.length < t.length) ans = '<';
+	else if (s.length > t.length) ans = '>';
+	else if (s.length == t.length){
+		let eq = true;
+		for (let i=0; i<s.length; i++){
+			if (s[i] != t[i]) {
+				if (s[i] < t[i]) ans = '<';
+				else if (s[i] > t[i]) ans = '>';
+				eq = false;
+				break;
+			}
+		}
+		if (eq) ans = '=';
+	}
+	return ans;
+}
+
 function BigPow(b, e){
 	let ans = "1";
-	let i = "0";
 
-	while (i != e){
-		ans = BigMult(ans, b);
-		i = BigInc(i);
+	while (cmp(e,"0") == '>'){
+		if (BigPar(e) != 0) ans = BigMult(ans, b);
+		b = BigMult(b, b);
+		e = BigDivInt(e,2);
 	}
 
 	return ans;
@@ -66,5 +103,13 @@ function BigPrint(s){
 		if (i>0 && (s.length-i)%3 == 0) ans += ".";
 		ans += s[i];
 	}
+	return ans;
+}
+
+function BigNotation(s, n, bnot=false){
+	let ans = s[0]+',';
+	for (let i=1; i<=n && i<s.length; i++) ans += s[i];
+	if (!bnot) ans += " · 10<sup>"+(s.length-1)+"</sup>";
+	else ans += " · 10<sup>"+( BigPrint( (s.length-1).toString() ) )+"</sup>";
 	return ans;
 }
